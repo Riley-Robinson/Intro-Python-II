532b631ef6b56f59cfbbb4adba7e19cdcc341312
Riley <rrtoinfinity@gmail.com>
2020-08-06 16:46:51 -0400

    well i updated the lesson side of my files

--- lesson/store.py
+++ lesson/store.py
@@ -1,71 +1,13 @@
-
-import sys
-
-from user import User
-
 class Store:
-
     def __init__(self, name, departments):
         self.name = name
         self.departments = departments
-
-    def __str__(self)
-        print"welcome get your shit and get out"
-
+​
+    def __str__(self):
+        return f"Welcome to the Quarantine Store! Have a nice shopping experience!"
+​
     def print_departments(self):
-
-        for id, name in self.departments.items():
-            print(f"{id}: {name}")
-            print('\n')
-
-# user will pass in either 0 or 1 command-line arguments to the program
-
-num_args = len(sys.argv)
-
-# if they pass in 0, we'll default their money to 100 
-
-if num_args == 1:
-    user = User(100)
-
-# if they pass in 1 argument (a number), set their money to that amount 
-
-elif num_args == 2:
-    user = User(int(sys.argv[1]))
-
-else:
-    print("Usage: store.py [money]")
-    sys.exit(1)
-
-departments = {
-    23: "Groceries", 
-    9: "Books", 
-    13: "Electronics", 
-    7: "Clothes", 
-    15: "Toys" 
-}
-
-store = Store("Quarantine Store", departments)
-
-while True:
-
-    print(store)
-    print(user, '\n')
-
-    # print departments 
-    store.print_departments()
-    selection = input("Which department would you like to visit? ")
-
-    if selection == 'quit' or selection == 'q':
-        break
-
-    # expect user to type in a number that is read in as a string 
-    # parse it into an int, and then check if the int is a valid 
-    dep_num = int(selection)
-
-    if dep_num in departments:
-        # if it is, go to that department
-        print(f"\nYou picked department number {dep_num}, the {departments[dep_num]} department.\n")
-
-    else:
-        # otherwise, print a message saying that is not a valid department 
-        print("\nThat is not a valid department.\n")
+        for id in self.departments:
+            print(self.departments[id])
+        print()
+		
